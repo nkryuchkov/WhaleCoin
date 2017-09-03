fb0d893cfe93cd38c96f197eb2223cc5e6d644d7
Rhett Creighton <rhett@heyrhett.com>
2017-09-03 16:20:10 -0500

    genesis

--- params/config.go
+++ params/config.go
@@ -25,8 +25,8 @@ import (
 )
 
 var (
-	MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3") // Mainnet genesis hash to enforce below configs on
-	TestnetGenesisHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d") // Testnet genesis hash to enforce below configs on
+	MainnetGenesisHash = common.HexToHash("0x1af591618608e221082239153520c99a90ae08521c87431e2462cde846654b67") // Mainnet genesis hash to enforce below configs on
+	TestnetGenesisHash = common.HexToHash("0x1af591618608e221082239153520c99a90ae08521c87431e2462cde846654b67") // Testnet genesis hash to enforce below configs on
 )
 
 var (
@@ -37,7 +37,7 @@ var (
 		DAOForkBlock:    big.NewInt(1920000),
 		DAOForkSupport:  true,
 		EIP150Block:     big.NewInt(2463000),
-		EIP150Hash:      common.HexToHash("0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0"),
+		EIP150Hash:      common.HexToHash("0x1af591618608e221082239153520c99a90ae08521c87431e2462cde846654b67"),
 		EIP155Block:     big.NewInt(2675000),
 		EIP158Block:     big.NewInt(2675000),
 		MetropolisBlock: big.NewInt(math.MaxInt64), // Don't enable yet
@@ -52,7 +52,7 @@ var (
 		DAOForkBlock:    nil,
 		DAOForkSupport:  true,
 		EIP150Block:     big.NewInt(0),
-		EIP150Hash:      common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d"),
+		EIP150Hash:      common.HexToHash("0x1af591618608e221082239153520c99a90ae08521c87431e2462cde846654b67"),
 		EIP155Block:     big.NewInt(10),
 		EIP158Block:     big.NewInt(10),
 		MetropolisBlock: big.NewInt(math.MaxInt64), // Don't enable yet
